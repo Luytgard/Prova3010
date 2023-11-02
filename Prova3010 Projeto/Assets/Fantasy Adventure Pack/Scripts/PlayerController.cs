f27319fc58bff46377b503bf129b289ee6381bc0
Luytgard <133531814+Luytgard@users.noreply.github.com>
2023-11-01 23:40:17 -0300

    Animações de movimento 2
    Animações de movimentos e aparência inicial da primeira faze implementadas.

---
+++ Prova3010 Projeto/Assets/Fantasy Adventure Pack/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontalInput;
+    public float moveSpeed = 5f;
+    public float jumpPower = 6f;
+    bool isGrounded = false;
+    bool isFacingRight = false;
+    bool isFalling = false;
+
+    Rigidbody2D rb;
+    public Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");//Recebe o input.
+        FlipSprite();
+        IsGrounded();
+        IsFalling();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);//Transforma o input em movimento.
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput > 0f || !isFacingRight && horizontalInput < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+
+        }
+    }
+
+    private void IsGrounded()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+    }
+
+    private void IsFalling()
+    {
+        if(rb.velocity.y < -0.1)
+        {
+            animator.SetBool("isFalling", true);
+        }
+        else
+        {
+            animator.SetBool("isFalling", false);
+        }
+    }
+}
