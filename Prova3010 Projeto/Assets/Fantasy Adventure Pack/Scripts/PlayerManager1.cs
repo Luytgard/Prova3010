38c49450487cc2ee40a2a6beec7812cb82a7b261
Luytgard <133531814+Luytgard@users.noreply.github.com>
2023-11-06 01:56:06 -0300

    Coins na fase1

--- Prova3010 Projeto/Assets/Fantasy Adventure Pack/Scripts/PlayerManager1.cs
+++ Prova3010 Projeto/Assets/Fantasy Adventure Pack/Scripts/PlayerManager1.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerManager1 : MonoBehaviour
 {
     public static bool isGameOver;
     public GameObject gameOverScreen;
+    public static Vector2 lastCheckPointPosition = new Vector2(-9,-3);
+    public static int numberOfCoins;
+    public TextMeshProUGUI coinsText;
+
 
     private void Awake()
     {
         isGameOver = false;
+        GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPosition;
     }
     // Start is called before the first frame update
     void Start()
@@ -21,6 +27,8 @@ public class PlayerManager1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        coinsText.text = numberOfCoins.ToString();
+        
         if (isGameOver)
         {
             gameOverScreen.SetActive(true);
